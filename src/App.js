02940dc010b549b0320bc41937af698dfad1c865
EwaNikodem <ewanikodem96@gmail.com>
2024-01-09 21:16:34 +0100

    getting monsters from API and filtering an input

--- src/App.js
+++ src/App.js
@@ -24,23 +24,47 @@ import './App.css';
 // }
 
 class App extends Component {
+  constructor(){
+    super();
+
+    this.state = {
+      monsters: [], 
+  };
+  console.log('constructor');
+  }
+
+  componentDidMount() {
+    console.log('component did mount');
+    fetch('https://jsonplaceholder.typicode.com/users') 
+      .then((response) => response.json())
+      .then(users => this.setState(
+        () => { return {monsters: users} 
+      },
+      () => {
+        console.log(this.state)
+      }
+      )) //this.setState({monsters: users}))
+  }
+
   render() {
+    console.log('render');
     return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input className='search-box' type='search' placeholder='search monsters' onChange={(event) => {
+        console.log(event.target.value)
+        const searchString = event.target.value.toLocaleLowerCase()
+        const filteredMonsters = this.state.monsters.filter((monster) => {
+            return monster.name.toLocaleLowerCase().includes(searchString);
+        });
+
+         this.setState(() => {
+          return { monsters: filteredMonsters};
+        })
+      }}/>
+      {this.state.monsters.map( (monster) => {
+        return <div key={monster.id}><h1>{monster.name}</h1></div>
+      })
+      }
     </div>
   );
   }
